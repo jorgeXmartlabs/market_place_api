6b93827f793ec101b21eddf7aa44fcbbe25a1ab2
Jorge L <jorgeluis.jimenez@xmartlabs.com>
2022-01-21 17:16:15 -0300

    Add Postgres and fix minor issues in the User Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,21 +2,23 @@ module Api
   module V1
     # User Controller
     class UsersController < ApplicationController
+      wrap_parameters :user, include: %i[email password]
+
       before_action :set_user, only: %i[show update destroy]
 
       # GET /users/1
       def show
-        render json: User.find(params[:id])
+        render json: @user
       end
 
       # POST /users
       def create
-        @user = User.new(user_params)
+        user = User.new(user_params)
 
-        if @user.save
-          render json: @user, status: :created
+        if user.save
+          render json: user, status: :created
         else
-          render json: @user.errors, status: :unprocessable_entity
+          render json: user.errors, status: :unprocessable_entity
         end
       end
 
