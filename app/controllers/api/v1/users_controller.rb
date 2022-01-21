06faffa964fba4022c30309e90853352533c2336
Jorge L <jorgeluis.jimenez@xmartlabs.com>
2022-01-21 15:12:45 -0300

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     # User Controller
     class UsersController < ApplicationController
+      before_action :set_user, only: %i[show update]
+
       # GET /users/1
       def show
         render json: User.find(params[:id])
@@ -18,11 +20,24 @@ module Api
         end
       end
 
+      # PATCH/PUT /users/1
+      def update
+        if @user.update(user_params)
+          render json: @user, status: :ok
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
       private
 
       # Only allow a trusted parameter "white list" through.
       def user_params
         params.require(:user).permit(:email, :password)
+      end
+
+      def set_user
+        @user = User.find(params[:id])
       end
     end
   end
